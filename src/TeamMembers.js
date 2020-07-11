85d10156cb52418e3fbd4fd1e9a728eb59ce7ea3
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-07-11 08:54:02 -0700

    Adding styling

--- src/TeamMembers.js
+++ src/TeamMembers.js
@@ -3,10 +3,15 @@ import React from 'react';
 const TeamMembers = props => {
     return (
         <div className='teamMembersList'>
+                <div className='title'>
+                    <h2>Name</h2>
+                    <p className='middleTitle'>Email</p>
+                    <p>Role</p>
+                </div>
             {props.teamMembers.map((teamMember, i) => (
                 <div className='teamMember' key={i}>
                     <h2>{teamMember.name}</h2>
-                    <p>{teamMember.email}</p>
+                    <p className='middleTitle'>{teamMember.email}</p>
                     <p>{teamMember.role}</p>
                 </div>
             ))}
