f4586cda7acd409ddd51decbe26985a3cab7fd93
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-07-11 08:29:44 -0700

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
-import Form from 'Form';
-import TeamMembers from 'TeamMembers';
+import Form from './Form';
+import TeamMembers from './TeamMembers';
 
 function App() {
-  const defaultTeamMembersList = {name: '', email: '', role: ''};
-  const [teamMembers, setTeamMembers] = useState(defaultTeamMembersList);
+  const [teamMembers, setTeamMembers] = useState([]);
   
   return (
     <div className="App">
       <h1>Team Members</h1>
-      <Form teamMembers={teamMembers} setTeamMembers={setTeamMembers}/>
-      <TeamMembers teamMembers={teamMembers}/>
+      <Form teamMembers={teamMembers} setTeamMembers={setTeamMembers} />
+      <TeamMembers teamMembers={teamMembers} />
     </div>
   );
 };
